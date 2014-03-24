12d58a078ef79b8431ef6fc9d81529f87f1a8bcc
Matthew Wickman <wickman.matthew@gmail.com>
2014-03-24 13:25:57 -0400

    refactoring to use Maze object

--- maze.js
+++ maze.js
@@ -1,94 +1,98 @@
+var Maze = function(config){
+  var self = this;
+  /*
+    Bits for open square sides:
+    North = 1
+    South = 2
+    East  = 4
+    West  = 8
+  */
+  // initializing the grid bitfield
+  this.grid = new Array();
+  this.height = config.height || 20;
+  this.width = config.width || 20;
 
-/*
-  Bits for open square sides:
-  North = 1
-  South = 2
-  East  = 4
-  West  = 8
-*/
-// initializing the grid bitfield
-var grid = new Array();
-var height = 20;
-var width = 20;
-
-function clearGrid() {
-  for (var i=0; i<height; i++) {
-      grid[i] = new Array();
-  	for (var j=0; j < width; j++) {
-  		grid[i][j]=0
-  	}
+  function clearGrid() {
+     for (var i=0; i<self.height; i++) {
+         self.grid[i] = new Array();
+     	for (var j=0; j < self.width; j++) {
+     		self.grid[i][j]=0
+     	}
+     }
   }
-}
+  clearGrid();
 
-// array randomizer
-function fisherYates ( myArray ) {
-  var i = myArray.length, j, tempi, tempj;
-  if ( i === 0 ) return false;
-  while ( --i ) {
-     j = Math.floor( Math.random() * ( i + 1 ) );
-     tempi = myArray[i];
-     tempj = myArray[j];
-     myArray[i] = tempj;
-     myArray[j] = tempi;
-   }
-}
+  // array randomizer
+  function fisherYates ( myArray ) {
+    var i = myArray.length, j, tempi, tempj;
+    if ( i === 0 ) return false;
+    while ( --i ) {
+       j = Math.floor( Math.random() * ( i + 1 ) );
+       tempi = myArray[i];
+       tempj = myArray[j];
+       myArray[i] = tempj;
+       myArray[j] = tempi;
+     }
+  }
 
-// constants for algorithm
-var cardinal = {
-  n: 1,
-  s: 2,
-  e: 4,
-  w: 8,
-}
+  // constants for algorithm
+  var cardinal = {
+    n: 1,
+    s: 2,
+    e: 4,
+    w: 8,
+  }
+  self.cardinal = cardinal;
 
-var dx = {
-  e: 1,
-  w: -1,
-  n: 0,
-  s: 0
-}
+  var dx = {
+    e: 1,
+    w: -1,
+    n: 0,
+    s: 0
+  }
 
-var dy = {
-  e: 0,
-  w: 0,
-  n: -1,
-  s: 1
-}
-var opposite = {
-  e: 8,
-  w: 4,
-  n: 2,
-  s: 1
-}
+  var dy = {
+    e: 0,
+    w: 0,
+    n: -1,
+    s: 1
+  }
+  var opposite = {
+    e: 8,
+    w: 4,
+    n: 2,
+    s: 1
+  }
 
 
-//algorithm
-function carve(cx, cy, grid) {
-  var directions = ['n', 's', 'e', 'w'];
-	fisherYates (directions);
+  //algorithm
+  function carve(cx, cy, grid) {
+    var directions = ['n', 's', 'e', 'w'];
+  	fisherYates (directions);
 
-  for (var i=0; i<4; i++) {
+    for (var i=0; i<4; i++) {
 
-    var nx = cx + dx[directions[i]];
-    var ny = cy + dy[directions[i]];
+      var nx = cx + dx[directions[i]];
+      var ny = cy + dy[directions[i]];
 
-// this if statement is nasty because im trying to avoid outside libraries, so I have no 'between' method
-    if ( ((ny >= 0) && (ny <= grid.length - 1)) && ((nx >= 0) && (nx <= grid[ny].length - 1)) && (grid[ny][nx] ==0) ) {
-      grid[cy][cx] = grid[cy][cx] | cardinal[directions[i]];
-      grid[ny][nx] = grid[ny][nx] | opposite[directions[i]];
+  // this if statement is nasty because im trying to avoid outside libraries, so I have no 'between' method
+      if ( ((ny >= 0) && (ny <= grid.length - 1)) && ((nx >= 0) && (nx <= grid[ny].length - 1)) && (grid[ny][nx] ==0) ) {
+        grid[cy][cx] = grid[cy][cx] | cardinal[directions[i]];
+        grid[ny][nx] = grid[ny][nx] | opposite[directions[i]];
 
-      carve(nx, ny, grid);
+        carve(nx, ny, grid);
 
-    }
+      }
 
-  } 
-}
+    } 
+  }
 
-// generate the maze, do it!
-carve(0, 0, grid)
+  // generate the maze, do it!
+  carve(0, 0, self.grid)
 
-//showing final bitfield grid
-console.log("::Final Grid::");
-for (var i=0; i< grid.length; i++) {
-  console.log(grid[i]);
+  //showing final bitfield grid
+  console.log("::Final Grid::", self);
+  for (var i=0; i< self.grid.length; i++) {
+    console.log(self.grid[i]);
+  }
 }
